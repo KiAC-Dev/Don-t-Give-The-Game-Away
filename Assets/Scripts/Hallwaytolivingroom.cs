ebf8dbf7c03df1e8e7b429a890fb6b58b1b62648
Maddy Sutherland <maddysutherland.illustration@gmail.com>
2024-11-12 19:19:52 +0000

    Added presents
    And made some adjustments.

---
+++ Assets/Scripts/Hallwaytolivingroom.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this door will take the player to the living room
+public class Hallwaytolivingroom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
